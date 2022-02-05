01a3cabda07e86db590416936e79e99f70f46e22
nohvii <hvii.no.one@gmail.com>
2022-02-05 11:46:06 -1000

    Completed

--- BacligFrederick03.pl
+++ BacligFrederick03.pl
@@ -2,37 +2,66 @@ use strict;
 use warnings;
 
 # Perl Assignment 3
-# assignment done in the Perl implementing common programming concepts as
-# Program arguments, arrays, and subroutines
+#
+# perl script that will read in a text file of possible credit card numbers 
+# and attempt to categorize them to major credit card networks: 
+# American Express (Amex), Visa, Mastercard
+#
 # ICS 215
+#
 # Author: Fred Baclig
+
+# die message to notify user they entered the incorrect amount of arguments.
 my $dieMsg = "Error: Expecting 1 program argument. Found " . @ARGV . " instead.\n";
+# exits script if user does not enter exactly 1 argument
 $dieMsg = $dieMsg . "Usage: perl BacligFrederick03.pl filename\n";
 die "$dieMsg" if (@ARGV != 1);
+# store filename passed as a argument into variable
 my $inputFile = $ARGV[0];
+# name of file we will be writing the matched CC's too
+my $outputFile="matched_cc_numbers.txt";
+# variable used to temp store the read lines from file
+my $inputLine="";
+# array used to store the cc numbers when found
 my @ccArray="";
 
-# Used to temporarily store each line in the file as it is being read.
-my $inputLine = "";
+# Open a file handle for reading
+# Exits script if file cannot be opened
+open inFH, "<", $inputFile or die "Unable to open file: $inputFile\n";
 
-# Open a file handle for reading.
-open inFH, "<", $inputFile;
-
-# As long as the file has lines, store it in $inputLine, then print it out.
+# As long as the file has lines, store it in $inputLine, then process for cc numbers
 while ($inputLine = <inFH>) {
-  # Any processing to the line will occur here in the loop.
+
+  # matches regex for AMEX and adds to the end of the array if true.
   if($inputLine=~m/^3[47][0-9]{13}$/){
     push(@ccArray,"AMEX: $inputLine");
   }
+  # matches regex for VISA and adds to the end of the array if true.
   elsif($inputLine=~m/^4[0-9]{12}$|^4[0-9]{16}$/){
     push(@ccArray,"VISA: $inputLine");
   }
+  # matches regex for MASTERCARD adds to the end of the array if true.
   elsif($inputLine=~m/^5[1-6]\d{14}$|222[1-9]\d{12}$|2720\d{12}$/){
     push(@ccArray,"MASTERCARD: $inputLine");
   }
 }
 
-# Done reading.
+# done reading
 close inFH;
-my $arrayLength = @ccArray;
-print @ccArray
+
+# print matched cc numbers
+print @ccArray;
+
+# Done reading.
+# open a file handle for writing
+open outFH, ">", $outputFile;
+
+# writes matched cc numbers found in cc_numbers.txt to matched_cc_numbers.txt
+print outFH @ccArray;
+
+# finished writing
+close outFH;
+
+# pau!
+print "\n";
+print "Program done!";
---
+++ BacligFrederickTest.pl
@@ -0,0 +1,36 @@
+use strict;
+use warnings;
+
+# Perl Assignment 3
+# assignment done in the Perl implementing common programming concepts as
+# Program arguments, arrays, and subroutines
+# ICS 215
+# Author: Fred Baclig
+
+my $inputFile = "cc_numbers.txt";
+my @ccArray="";
+
+# Used to temporarily store each line in the file as it is being read.
+my $inputLine = "";
+
+# Open a file handle for reading.
+open inFH, "<", $inputFile;
+
+# As long as the file has lines, store it in $inputLine, then print it out.
+while ($inputLine = <inFH>) {
+  # Any processing to the line will occur here in the loop.
+  if($inputLine=~m/^3[47][0-9]{13}$/){
+    push(@ccArray,"AMEX: $inputLine");
+  }
+  elsif($inputLine=~m/^4[0-9]{12}$|^4[0-9]{16}$/){
+    push(@ccArray,"Mastercard: $inputLine");
+  }
+  elsif($inputLine=~m/^5[1-6]\d{14}$|222[1-9]\d{12}$|2720\d{12}$/){
+    push(@ccArray,"Visa: $inputLine");
+  }
+}
+
+# Done reading.
+close inFH;
+my $arrayLength = @ccArray;
+print @ccArray;
