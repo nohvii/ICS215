3145294491071e8037b4deec7bc40dab719d33c4
nohvii <hvii.no.one@gmail.com>
2022-02-08 19:47:51 -1000

    Assignment 04 prototype

--- BacligFrederickTest.pl
+++ BacligFrederickTest.pl
@@ -1,40 +1,161 @@
 use strict;
 use warnings;
 
-# Perl Assignment 3
-# assignment done in the Perl implementing common programming concepts as
-# Program arguments, arrays, and subroutines
-# ICS 215
-# Author: Fred Baclig
 
-my $inputFile = "cc_numbers.txt";
-my @ccArray="";
+my $forecast="td CLASS=\"default\"><a href=\"./avail.class?i=LEE&t=202230&c=55179\">55179</a></td>
+<td nowrap=\"nowrap\" CLASS=\"default\">ICS 101</td>
+<td CLASS=\"default\">0</td>
+<!-- ssrmeet_array_length = 0 -->
+<!-- ssrmeet_array_length = 1 -->
+<!-- comment_text=Textbook Cost: $0.  <br>Students must use Microsoft Applications designed to run on the Windows or Mac Operating System which is available for a free download.  The online version of Microsoft Applications is not sufficient for this course.<br>Students will be contacted at their UH email address by their instructor prior to the first day of class. <br> -->
+<td CLASS=\"default\">Digital Tools for Info World</td>
+<td CLASS=\"default\">3</td>
+<td CLASS=\"default\"><abbr title=\"Matthew C French\"><span class=\"abbr\" title=\"Matthew C French\">M C French</span></abbr></td>
+<td class=\"default\" align=\"center\">20</td>
+<td class=\"default\" align=\"center\">0</td>
+<td CLASS=\"default\">TBA</td>
+<td CLASS=\"default\">TBA</td>
+<td CLASS=\"default\"><abbr title=\"World Wide Web \"><span class=\"abbr\" title=\"World Wide Web \">WWW </span></abbr></td>
+<td CLASS=\"default\">01/10-05/13</td>
+</tr>
+<tr class=\"odd\">
+<td colspan=\"13\">
+Textbook Cost: $0.  <br>Students must use Microsoft Applications designed to run on the Windows or Mac Operating System which is available for a free download.  The online version of Microsoft Applications is not sufficient for this course.<br>Students will be contacted at their UH email address by their instructor prior to the first day of class. <br>
+</td>
+</tr>
+<!-- sect_rec: i 'LEE', term: '202230', ssbsect_crn: '55207', ssrmeet: '55207' -->
+<!-- pt. a -->
+<!-- pt. b -->
+<!-- pt. c -->
+<!-- pt. d -->
+<!-- pt. e -->
+<!-- pt. f -->
+<!-- pt. g -->
+<tr>
+<td CLASS=\"default\">C19,TXT0</td>
+<!-- pt. h -->
+<td CLASS=\"default\"><a href=\"./avail.class?i=LEE&t=202230&c=55207\">55207</a></td>
+<td nowrap=\"nowrap\" CLASS=\"default\">ICS 101</td>
+<td CLASS=\"default\">0</td>
+<!-- ssrmeet_array_length = 0 -->
+<!-- ssrmeet_array_length = 1 -->
+<!-- comment_text=Textbook Cost: $0.  <br>This course meets the VA face-to-face requirement due to COVID-19.<br>Students must use Microsoft Applications designed to run on the Windows or Mac Operating System which is available for a free download.  The online version of Microsoft Applications is not sufficient for this course.<br>Students will be contacted at their UH email address by their instructor prior to the first day of class. <br> -->
+<td CLASS=\"default\">Digital Tools for Info World</td>
+<td CLASS=\"default\">3</td>
+<td CLASS=\"default\"><abbr title=\"Norman Hines\"><span class=\"abbr\" title=\"Norman Hines\">N Hines</span></abbr></td>
+<td class=\"default\" align=\"center\">19</td>
+<td class=\"default\" align=\"center\">1</td>
+<td CLASS=\"default\">TBA</td>
+<td CLASS=\"default\">TBA</td>
+<td CLASS=\"default\"><abbr title=\"World Wide Web \"><span class=\"abbr\" title=\"World Wide Web \">WWW </span></abbr></td>
+<td CLASS=\"default\">01/10-05/13</td>
+</tr>
+<tr>
+<td colspan=\"13\">
+Textbook Cost: $0.  <br>This course meets the VA face-to-face requirement due to COVID-19.<br>Students must use Microsoft Applications designed to run on the Windows or Mac Operating System which is available for a free download.  The online version of Microsoft Applications is not sufficient for this course.<br>Students will be contacted at their UH email address by their instructor prior to the first day of class. <br>
+</td>
+</tr>
+<!-- sect_rec: i 'LEE', term: '202230', ssbsect_crn: '55354', ssrmeet: '55354' -->
+<!-- pt. a -->
+<!-- pt. b -->
+<!-- pt. c -->
+<!-- pt. d -->
+<!-- pt. e -->
+<!-- pt. f -->
+<!-- pt. g -->
+<tr class=\"odd\">
+<td CLASS=\"default\">C19,TXT0</td>
+<!-- pt. h -->
+<td CLASS=\"default\"><a href=\"./avail.class?i=LEE&t=202230&c=55354\">55354</a></td>
+<td nowrap=\"nowrap\" CLASS=\"default\">ICS 101</td>
+<td CLASS=\"default\">0</td>
+<!-- ssrmeet_array_length = 0 -->
+<!-- ssrmeet_array_length = 1 -->
+<!-- comment_text=Textbook Cost: $0.  <br>This course meets the VA face-to-face requirement due to COVID-19.<br>Students must use Microsoft Applications designed to run on the Windows or Mac Operating System which is available for a free download.  The online version of Microsoft Applications is not sufficient for this course.<br>Students will be contacted at their UH email address by their instructor prior to the first day of class. <br><br> -->
+<td CLASS=\"default\">Digital Tools for Info World</td>
+<td CLASS=\"default\">3</td>
+<td CLASS=\"default\"><abbr title=\"Thomas D Burns\"><span class=\"abbr\" title=\"Thomas D Burns\">T D Burns</span></abbr></td>
+<td class=\"default\" align=\"center\">20</td>
+<td class=\"default\" align=\"center\">0</td>
+<td CLASS=\"default\">TBA</td>
+<td CLASS=\"default\">TBA</td>
+<td CLASS=\"default\"><abbr title=\"World Wide Web \"><span class=\"abbr\" title=\"World Wide Web \">WWW </span></abbr></td>
+<td CLASS=\"default\">01/10-05/13</td>
+</tr>
+<tr class=\"odd\">
+<td colspan=\"13\">
+Textbook Cost: $0.  <br>This course meets the VA face-to-face requirement due to COVID-19.<br>Students must use Microsoft Applications designed to run on the Windows or Mac Operating System which is available for a free download.  The online version of Microsoft Applications is not sufficient for this course.<br>Students will be contacted at their UH email address by their instructor prior to the first day of class. <br><br>
+</td>
+</tr>
+<!-- sect_rec: i 'LEE', term: '202230', ssbsect_crn: '55632', ssrmeet: '55632' -->
+<!-- pt. a -->
+<!-- pt. b -->
+<!-- pt. c -->
+<!-- pt. d -->
+<!-- pt. e -->
+<!-- pt. f -->
+<!-- pt. g -->
+<tr>
+<td CLASS=\"default\">C19,TXT0</td>
+<!-- pt. h -->
+<td CLASS=\"default\"><a href=\"./avail.class?i=LEE&t=202230&c=55632\">55632</a></td>
+<td nowrap=\"nowrap\" CLASS=\"default\">ICS 101</td>
+<td CLASS=\"default\">0</td>
+<!-- ssrmeet_array_length = 0 -->
+<!-- ssrmeet_array_length = 1 -->
+<!-- comment_text=Textbook Cost: $0.<br>This course meets the VA face-to-face requirement due to COVID-19.<br>Students must use Microsoft Applications designed to run on the Windows or Mac Operating System which is available for a free download. The online version of Microsoft Applications is not sufficient for this course.<br>Students will be contacted at their UH email address by their instructor prior to the first day of class.<br> -->
+<td CLASS=\"default\">Digital Tools for Info World</td>
+<td CLASS=\"default\">3</td>
+<td CLASS=\"default\"><abbr title=\"Norman Hines\"><span class=\"abbr\" title=\"Norman Hines\">N Hines</span></abbr></td>
+<td class=\"default\" align=\"center\">18</td>
+<td class=\"default\" align=\"center\">2</td>
+<td CLASS=\"default\">TBA</td>
+<td CLASS=\"default\">TBA</td>
+<td CLASS=\"default\"><abbr title=\"World Wide Web \"><span class=\"abbr\" title=\"World Wide Web \">WWW </span></abbr></td>
+<td CLASS=\"default\">01/10-05/13</td>
+</tr>
+<tr>
+<td colspan=\"13\">
+Textbook Cost: $0.<br>This course meets the VA face-to-face requirement due to COVID-19.<br>Students must use Microsoft Applications designed to run on the Windows or Mac Operating System which is available for a free download. The online version of Microsoft Applications is not sufficient for this course.<br>Students will be contacted at their UH email address by their instructor prior to the first day of class.<br>
+</td>
+</tr>
+<!-- sect_rec: i 'LEE', term: '202230', ssbsect_crn: '55975', ssrmeet: '55975' -->
+<!-- pt. a -->
+<!-- pt. b -->
+<!-- pt. c -->
+<!-- pt. d -->
+<!-- pt. e -->
+<!-- pt. f -->
+<!-- pt. g -->
+<tr class=\"odd\">
+<td CLASS=\"default\">TXT0</td>
+<!-- pt. h -->
+<td CLASS=\"default\"><a href=\"./avail.class?i=LEE&t=202230&c=55975\">55975</a></td>
+<td nowrap=\"nowrap\" CLASS=\"default\">ICS 101</td>
+<td CLASS=\"default\">0</td>
+<!-- ssrmeet_array_length = 0 -->
+<!-- ssrmeet_array_length = 1 -->
+<!-- comment_text=Textbook Cost: $0.  <br>Students must use Microsoft Applications designed to run on the Windows or Mac Operating System which is available for a free download.  The online version of Microsoft Applications is not sufficient for this course.<br>Students will be contacted at their UH email address by their instructor prior to the first day of class. <br><br> -->
+<td CLASS=\"default\">Digital Tools for Info World</td>
+<td CLASS=\"default\">3</td>
+<td CLASS=\"default\"><abbr title=\"Courtney N Kinder\"><span class=\"abbr\" title=\"Courtney N Kinder\">C N Kinder</span></abbr></td>
+<td class=\"default\" align=\"center\">19</td>
+<td class=\"default\" align=\"center\">1</td>
+<td CLASS=\"default\">TBA</td>
+<td CLASS=\"default\">TBA</td>
+<td CLASS=\"default\"><abbr title=\"World Wide Web \"><span class=\"abbr\" title=\"World Wide Web \">WWW </span></abbr></td>
+<td CLASS=\"default\">01/10-05/13</td>
+</tr>
+<tr class=\"odd\">
+<td colspan=\"13\">
+Textbook Cost: $0.  <br>Students must use Microsoft Applications designed to run on the Windows or Mac Operating System which is available for a free download.  The online version of Microsoft Applications is not sufficient for this course.<br>Students will be contacted at their UH email address by their instructor prior to the first day of class. <br><br>
+</td>
+</tr>";
+my $theClass="101";
+#if ($forecast=~m#<td nowrap=\"nowrap\" CLASS=\"default\">ICS\s(\d\d\d)</td#){
+# print "found $1";
+#}
 
-# Used to temporarily store each line in the file as it is being read.
-my $inputLine = "";
-
-# Open a file handle for reading.
-open inFH, "<", $inputFile;
-
-# As long as the file has lines, store it in $inputLine, then print it out.
-while ($inputLine = <inFH>) {
-  # Any processing to the line will occur here in the loop.
-  if($inputLine=~m/^3[47][0-9]{13}$/){
-    push(@ccArray,"AMEX: $inputLine");
-  }
-  elsif($inputLine=~m/^4[0-9]{12}$|^4[0-9]{16}$/){
-    push(@ccArray,"Mastercard: $inputLine");
-  }
-  elsif($inputLine=~m/^5[1-6]\d{14}$|222[1-9]\d{12}$|2720\d{12}$/){
-    push(@ccArray,"Visa: $inputLine");
-  }
-}
-
-# Done reading.
-close inFH;
-my $arrayLength = @ccArray;
-print @ccArray;
-while ($inputLine = <inFH>) {
-  # Any processing to the line will occur here in the loop.
-  print $inputLine;
+while($forecast=~m#101<\/td>.+?<td class=\"default\" align=\"center\">\d{1,2}.+?<td class+=\"default\" align=\"center\">(\d{1,2})#gs){
+  print "$1 \n";
 }
